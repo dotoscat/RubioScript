166d3dd5adb9740d0bcc141ee01d2d714a47be17
Oscar Triano dotoscat <user@example.com>
2017-07-16 13:20:37 +0200

    Obtener asignaciones

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,22 @@
 "use strict";
 
+/*
+
+Definición informal del lenguaje
+
+Cada instruccion está separada por punto.
+
+<sujeto>(tiene)<n><objeto> =
+
+Ejemplo: "Maria tiene 2 manzana. Si se come 2. Cuántas tendrá?"
+
+*/
+
+const asignacion = /(\w+)\s+(tiene)\s+(\d+)\s+(\w+)/
+
 function ejecutar(codigo) {
-    console.log(codigo);
+    const codigo_separado = codigo.split('.');
+    const codigo_limpio = codigo_separado.map((linea_codigo) => linea_codigo.trim());
+    const instrucciones = codigo_limpio.map((instruccion) => asignacion.exec(instruccion));
+    console.log(instrucciones);
 }
