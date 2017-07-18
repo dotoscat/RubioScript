036c912144979bf8c7d881b074cb23246e7d346d
Oscar Triano dotoscat <user@example.com>
2017-07-18 22:32:51 +0200

    Convertir en instrucciones

--- interpreter.js
+++ interpreter.js
@@ -16,10 +16,15 @@ let sujetos = new Map();
 let objetos = new Map();
 
 const asignacion = /(\w+)\s+(tiene)\s+(\d+)\s+(\w+)/
+const respuesta = /(Cuantos|Cuantas)\s+(\w+)\s+tiene(\w+)\?/
+
+let relaciones_operacion = new Map();
+relaciones_operacion.set(asignacion, operacion_asignacion);
+relaciones_operacion.set(respuesta, operacion_respuesta);
 
 function operacion_asignacion(instruccion){
-    const sujeto = instruccion[1]
-    const verbo = instruccion[2]
+    const sujeto = instruccion[1];
+    const verbo = instruccion[2];
     const cantidad = Number(instruccion[3]);
     const objeto = instruccion[4];
     if (!sujetos.has(sujeto)){
@@ -32,17 +37,30 @@ function operacion_asignacion(instruccion){
     objetos.set(objeto, objetos.get(objeto) + cantidad);
 }
 
+function operacion_respuesta(instruccion){
+    console.log("Una respuesta");
+}
+
 function preparar_codigo(codigo) {
     const codigo_separado = codigo.split('.');
     const codigo_limpio = codigo_separado.map((linea_codigo) => linea_codigo.trim());
-    const instrucciones = codigo_limpio.map((instruccion) => asignacion.exec(instruccion));
+    const instrucciones = codigo_limpio.map((instruccion) => {
+        for (let operacion of relaciones_operacion){
+            let encuentro = operacion[0].exec(instruccion);
+            if (encuentro === null) continue;
+            return {operacion: operacion, encuentro: encuentro};
+        }
+        return null;
+    });
     return instrucciones;
 }
 
 function ejecutar(codigo) {
     const instrucciones = preparar_codigo(codigo);
     for (let instruccion of instrucciones) {
-        operacion_asignacion(instruccion);
+        if (instruccion === null) continue;
+        console.log(instruccion);
+        //operacion_asignacion(instruccion);
     }
     console.log(sujetos);
     console.log(objetos);
